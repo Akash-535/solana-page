40cd4d11818e9579f0b33467b2073608f431895d
akash-535 <akashbish9e@gmail.com>
2024-07-26 08:59:10 +0530

    preloader added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-nvabar")
     document.body.classList.toggle("overflow-hidden")
+    document.getElementById("lines").classList.toggle("line-1-1")
+    document.getElementById("lines-2").classList.toggle("line-2")
+    document.getElementById("lines-3").classList.toggle("line-3")
+
 }
+
+
+    var preloader = document.getElementById("loader")
+
+    function loading(){
+        preloader.style.display = "none";
+        setTimeout(function() {
+            elapsed = true;
+            if (loaded)
+                hideLoadingScreen();
+        }, 50000);
+    }
+   
